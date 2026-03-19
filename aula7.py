cd82652d4503a12803d972c9655eae9a068340d0
Bortoli07 <cayo.bortoli@escola.pr.gov.br>
2026-03-19 08:06:01 -0300

    Create aula7.py

---
+++ aula7.py
@@ -0,0 +1,3 @@
+n1=int(input('digite um numero'))
+print('o numero digitado foi{} o dobro é{}, seu triplo {} e a raiz quadrada é {:.2f}'. format (n1,n1*2,n1*3,n1**0.5))
+#para calular a raiz quadrada posso ultilizar a função pow(n**0.512)
