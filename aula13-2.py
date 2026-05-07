b3e113066e1e0e69a4b7b2a199354dfebfe63d19
Bortoli07 <cayo.bortoli@escola.pr.gov.br>
2026-05-07 08:10:16 -0300

    Create aula13-2.py

---
+++ aula13-2.py
@@ -0,0 +1,9 @@
+num = int(input('Digite um número entre 0 e 9999: '))
+unidade = num // 1 % 10
+dezena = num // 10 % 10
+centena = num // 100 % 10
+milhar = num // 1000 % 10
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')
